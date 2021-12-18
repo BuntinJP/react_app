24729eab687eb87bf35c600202c4bb9dccbeb591
Aoki Takumi <aokitakumi72@gmail.com>
2021-12-18 10:46:17 +0900

    oge

--- src/App.js
+++ src/App.js
@@ -3,8 +3,11 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      {/* <header className="App-header">
+    <div>
+      <h1>Hello, World!</h1>
+    </div>
+    /* <div className="App">
+      <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -17,11 +20,9 @@ function App() {
         >
           Learn React
         </a>
-      </header> */}
-      <div>
-        
-      </div>
-    </div>
+      </header> 
+      
+    </div> */
   );
 }
 
